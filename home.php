b92e98a83271c6df39fae9d396da854a0b1c32ad
jeremy <jeremy.nowak@laplateforme.io>
2024-03-12 15:27:28 +0100

    work in progress

--- home.php
+++ home.php
@@ -4,32 +4,85 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <script src="https://cdn.tailwindcss.com"></script>
+
     <title>Document</title>
 </head>
 <body>
-    <p>Coucou!</p>
-    <h1 x-data="{ message: 'I ❤️ Alpine' }" x-text="message"></h1>
 
-    <div x-data="{ count: 0 }">
-        <button x-on:click="count++">Increment</button>
-        <span x-text="count"></span>
-    </div>  
+    <p>Coucou!</p>
+
+
+    <div x-data="{
+    products: [],
+    currentPage: 1,
+    itemsPerPage: 10,
+    paginatedData() {
+        const start = (this.currentPage - 1) * this.itemsPerPage;
+        const end = start + this.itemsPerPage;
+        return this.products.slice(start, end);
+    },
+    nextPage() {
+        if (this.currentPage < this.totalPages) {
+            this.currentPage++;
+        }
+    },
+    prevPage() {
+        if (this.currentPage > 1) {
+            this.currentPage--;
+        }
+    },
+    totalPages() {
+        return Math.ceil(this.products.length / this.itemsPerPage);
+    }
+}" x-init="fetch('https://world.openfoodfacts.net/api/v2/search')
+      .then(response => response.json())
+      .then(data => products = data.products)">
+
+    <!-- Affichage des produits de la page courante -->
+    <ul>
+        <template x-for="product in paginatedData()" :key="product.id">
+            <li x-text="product.name"></li>
+        </template>
+
+        
+    </ul>
+
+    <template x-for="product in paginatedData()">
+            <div>
+                <div x-text="product.product_name"></div>
+                <img :src="product.image_url" alt=""> 
+                <div x-text="product.allergens"></div>
+                <button :id="product._id" >Add to favorite</button>
+            </div>
+        </template>
+    <!-- Pagination -->
+    <button @click="prevPage()" :disabled="currentPage === 1">Previous</button>
+    <button @click="nextPage()" :disabled="currentPage === totalPages()">Next</button>
+
+</div>
+
 
     
-    <div
-           x-data="{products: []}"
-           x-init="fetch('https://world.openfoodfacts.net/api/v2/product/3017624010701')
-                      .then(response => response.json())
-                      .then(data => products = data)">
+    <div x-data="{ products: [],
+                    currentPage: 1,
+                    itemsPerPage: 10
+                        
+    }" x-init="fetch('https://world.openfoodfacts.net/api/v2/search')
+      .then(response => response.json())
+      .then(data => products = data.products)">
     <div>
-      <template x-for="product in products">
+        <template x-for="product in products">
+            <div>
+                <div x-text="product.product_name"></div>
+                <img :src="product.image_url" alt=""> 
+                <div x-text="product.allergens"></div>
+                <button :id="product._id" >Add to favorite</button>
+            </div>
+        </template>
 
-                <div x-text="product.id"></div>
-
-      </template>
     </div>
 </div>
+
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,16 +6,34 @@ use App\Controller\UserController;
 
 
 $router = new AltoRouter();
+// Chemin de base utilisé par vos collègues
+$colleaguesBasePath = '/pomme-d-api';
+// Chemin de base que vous utilisez
+$yourBasePath = '/plateforme/pomme-d-api';
+
+// Récupérer l'URI actuelle
+$uri = $_SERVER['REQUEST_URI'];
+
+// Vérifier si l'URI commence par le chemin de base des collègues
+if (strpos($uri, $colleaguesBasePath) === 0) {
+    // Si c'est le cas, remplacer le chemin de base par le vôtre
+    $uri = $yourBasePath . substr($uri, strlen($colleaguesBasePath));
+    $router ->setBasePath($yourBasePath);
+}
+
 $router ->setBasePath('/pomme-d-api');
+
+    
+
 
 
 $router->map('GET', '/', function() {
     require "home.php";
 }, "home");
 
-
 $router->map('GET', '/register', function() {
     require "./src/View/register.php";
+    
 }, "register");
 
 $router->map('POST', '/register', function() {
@@ -31,6 +49,9 @@ $router->map('POST', '/login', function() {
     $controller = new UserController();
     $controller->login($_POST["login"], $_POST["password"]);
 });
+
+
+
 
 $match = $router->match();
 
