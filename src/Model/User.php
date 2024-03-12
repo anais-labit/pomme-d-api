f0f95fd27e935a15d4e0779027f12ad419c51b07
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2024-03-12 11:01:35 +0100

    authenfication

---
+++ src/Model/User.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Model;
+use \PDO;
+
+
+Class User{
+    private $pdo;
+
+    public function __construct(){
+
+        $this->pdo = new \PDO("mysql:host=localhost;dbname=pomme_d_api", "root", "root");
+    }
+
+    public function create($login, $password){
+       $smt= $this->pdo->prepare("INSERT INTO user (login, password) VALUES (:login, :password)");
+         $smt->execute([
+              ":login" => $login,
+              ":password" => $password
+         ]);
+        
+    }
+
+    public function select ($login , $password=null){
+        $smt = $this->pdo->prepare("SELECT * FROM user WHERE login = :login");
+        $smt->execute([
+            ":login" => $login
+        ]);
+        $result = $smt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+}
+
+?>
