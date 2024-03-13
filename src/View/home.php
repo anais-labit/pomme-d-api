66ac1ee1cd127beb80d23305daac000b79ed4cb4
jeremy <jeremy.nowak@laplateforme.io>
2024-03-13 14:48:43 +0100

    Style in progress

--- src/View/home.php
+++ src/View/home.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link rel="stylesheet" href="public/style.css">
     <title>Document</title>
 </head>
 
