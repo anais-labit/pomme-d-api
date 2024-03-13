69a1eea3cc04cf9e52fcff9dccd64689efa5968d
jeremy <jeremy.nowak@laplateforme.io>
2024-03-13 14:59:31 +0100

    fixing

--- src/View/home.php
+++ src/View/home.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <link rel="stylesheet" href="public/style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="public/style/style.css">
     <title>Document</title>
 </head>
 
