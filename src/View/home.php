18caad7bdbd1ccb0e0598191d088625cd2f8b65d
anais-labit <anais.labit@gmail.com>
2024-03-13 12:03:34 +0100

    fix api

--- src/View/home.php
+++ src/View/home.php
@@ -1,11 +1,3 @@
-<?php
-// Autoriser les requêtes CORS depuis n'importe quelle origine
-header("Access-Control-Allow-Origin: *");
-
-// Autoriser les méthodes HTTP spécifiques
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,37 +9,44 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 </head>
 
 <body>
-<style>
-    [x-cloak]{
-        display: none !important;
-    }   
-</style>
-    <div x-data="{
-        products: [],
-        currentPage: 1,
-        itemsPerPage: 5,
-        showProductInfo: false,
-        paginatedData() {
-            const start = (this.currentPage - 1) * this.itemsPerPage;
-            const end = start + this.itemsPerPage;
-            return this.products.slice(start, end);
-        },
-        nextPage() {
-            if (this.currentPage < this.totalPages()) {
-                this.currentPage++;
-            }
-        },
-        prevPage() {
-            if (this.currentPage > 1) {
-                this.currentPage--;
-            }
-        },
-        totalPages() {
-            return Math.ceil(this.products.length / this.itemsPerPage);
+    <style>
+        [x-cloak] {
+            display: none !important;
         }
-    }" x-init="fetch('https://world.openfoodfacts.net/api/v2/search')
+    </style>
+    <div x-data="{
+    products: [],
+    currentPage: 1,
+    itemsPerPage: 5,
+    showProductInfo: {},
+    fetchProducts() {
+        fetch('https://world.openfoodfacts.org/api/v2/search')
             .then(response => response.json())
-            .then(data => products = data.products)">
+            .then(data => this.products = data.products);
+    },
+    paginatedData() {
+        const start = (this.currentPage - 1) * this.itemsPerPage;
+        const end = start + this.itemsPerPage;
+        return this.products.slice(start, end);
+    },
+    toggleProductInfo(productId) {
+        if (this.showProductInfo.hasOwnProperty(productId)) {
+            this.showProductInfo[productId] = !this.showProductInfo[productId];
+        } else {
+            this.showProductInfo[productId] = true; // Utilisation de l'opérateur d'assignation
+        }
+    },
+    nextPage() {
+        if (this.currentPage < Math.ceil(this.products.length / this.itemsPerPage)) {
+            this.currentPage++;
+        }
+    },
+    prevPage() {
+        if (this.currentPage > 1) {
+            this.currentPage--;
+        }
+    }
+}" x-init="fetchProducts()">
 
         <!-- Affichage des produits de la page courante -->
         <ul>
@@ -56,50 +55,38 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
                     <div x-text="product.product_name"></div>
                     <img :src="product.image_url" alt="">
                     <div x-text="product.allergens"></div>
-                    <button :id="product._id" x-data="showProductInfo= ! showProductInfo">Show infos</button>
+                    <button @click="toggleProductInfo(product._id)">Show infos</button>
                     <button :id="product._id">Add to favorite</button>
-
                 </li>
             </template>
         </ul>
 
-        
-        
+        <!-- Bloc pour afficher les informations supplémentaires -->
+        <template x-for="(show, productId) in showProductInfo" :key="productId">
+            <div x-show="show" x-cloak x-transition.opacity>
+                <div class="popup-background" style="background-color: rgba(0, 0, 0, 0.9); width: 100%; height: 100%;">
+                    <div class="popup-content" style="background-color: wheat; width: 50%; height: 50%; z-index: 101;">
+                        <button @click="toggleProductInfo(productId)">X</button>
+                        <h2>Product info</h2>
+                        <p>Product name: <span x-text="products.find(p => p._id === productId).product_name"></span></p>
+                        <p>Brand: <span x-text="products.find(p => p._id === productId).brands"></span></p>
+                        <p>Quantity: <span x-text="products.find(p => p._id === productId).quantity"></span></p>
+                        <p>Ingredients: <span x-text="products.find(p => p._id === productId).ingredients_text"></span></p>
+                        <p>Allergens: <span x-text="products.find(p => p._id === productId).allergens"></span></p>
+                        <p>Labels: <span x-text="products.find(p => p._id === productId).labels"></span></p>
+                        <p>Stores: <span x-text="products.find(p => p._id === productId).stores"></span></p>
+                        <p>Country: <span x-text="products.find(p => p._id === productId).countries"></span></p>
+                    </div>
+                </div>
+            </div>
+        </template>
+
         <!-- Pagination -->
         <button @click="prevPage()" :disabled="currentPage === 1">Previous</button>
-        <span x-text="`Page ${currentPage} of ${totalPages()}`"></span>
-        <button @click="nextPage()" :disabled="currentPage === totalPages()">Next</button>
-        
-        
-        
-    </div>
-    <div x-data="{ showProductInfo: false,
-    }">
-    <button @click="showProductInfo = true">Show infos</button>
+        <span x-text="`Page ${currentPage} of ${Math.ceil(products.length / itemsPerPage)}`"></span>
+        <button @click="nextPage()" :disabled="currentPage === Math.ceil(products.length / itemsPerPage)">Next</button>
 
-    <div 
-        x-show="showProductInfo" 
-        x-cloak 
-        x-transition.opacity
-        x-bind:style="z-index: 100"
-    >
-
-        <div  class="popup-background"         x-bind:style="z-index: 101" style="background-color: rgba(0, 0, 0, 0.900); width: 100%; height: 100%">
-        <div class="popup-content"         x-bind:style="z-index: 102"  style="background-color: wheat; width: 50%; height: 50%; " >
-            <button @click="showProductInfo = false">X</button>
-            <h2>Product info</h2>
-            <p>Product name: <span x-text="product.product_name"></span></p>
-            <p>Brand: <span x-text="product.brands"></span></p>
-            <p>Quantity: <span x-text="product.quantity"></span></p>
-            <p>Ingredients: <span x-text="product.ingredients_text"></span></p>
-            <p>Allergens: <span x-text="product.allergens"></span></p>
-            <p>Labels: <span x-text="product.labels"></span></p>
-            <p>Stores: <span x-text="product.stores"></span></p>
-            <p>Country: <span x-text="product.countries"></span></p>
-        </div>
-        </div>
     </div>
-</div>
 
 </body>
 
